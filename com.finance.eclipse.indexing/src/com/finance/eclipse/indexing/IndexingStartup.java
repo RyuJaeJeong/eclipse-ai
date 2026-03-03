fb90f4395ddf393973366c236e05a5269994d4e7
DGB\3240133 <fb0982@naver.com>
2026-03-03 17:59:35 +0900

    feat: indexing 로직 추가 2026-03-03 류재정 프로

--- com.finance.eclipse.indexing/src/com/finance/eclipse/indexing/IndexingStartup.java
+++ com.finance.eclipse.indexing/src/com/finance/eclipse/indexing/IndexingStartup.java
@@ -2,11 +2,19 @@ package com.finance.eclipse.indexing;
 
 import org.eclipse.ui.IStartup;
 
+import com.finance.eclipse.indexing.handlers.IndexingHandler;
+
 public class IndexingStartup implements IStartup {
 
 	@Override
 	public void earlyStartup() {
-		IndexingActivator.log().info("Hello, world!");
+		IndexingActivator.log().info("Indexing started from earlyStartup");
+		IndexingHandler handler = new IndexingHandler();
+		try {
+			handler.execute(null);
+		}catch (Exception e) {
+			IndexingActivator.log().error("Failed to execute indexing handler at startup", e);
+		}
 	}
 
 }
